e980f33b00b87838d6d4656bb273b156e42999f6
kcheykim <kosalcheykim@gmail.com>
2022-02-09 15:16:44 -0800

    fixing server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express'); //import ApolloServer
-const { typeDefs, resolvers } = require('./server/schemas'); //import our typeDefs and resolvers
-const db = require('./server/config/connection');
-const { authMiddleware } = require('./server/utils/auth'); //import the authentication
+const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas'); //import our typeDefs and resolvers
+const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth'); //import the authentication
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -23,6 +25,15 @@ startServer() //initialize the Apollo server
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
