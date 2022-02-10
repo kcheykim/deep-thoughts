6211b84932d711b31874f42d453418d6f61ed0ad
kcheykim <kosalcheykim@gmail.com>
2022-02-09 23:42:20 -0800

    json package fix

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -23,14 +23,14 @@ const ThoughtList = ({ thoughts, title }) => {
               thought on {thought.createdAt}
             </p>
             <div className="card-body">
-  <Link to={`/thought/${thought._id}`}>
-    <p>{thought.thoughtText}</p>
-    <p className="mb-0">
-      Reactions: {thought.reactionCount} || Click to{' '}
-      {thought.reactionCount ? 'see' : 'start'} the discussion!
-    </p>
-  </Link>
-</div>
+              <Link to={`/thought/${thought._id}`}>
+                <p>{thought.thoughtText}</p>
+                <p className="mb-0">
+                  Reactions: {thought.reactionCount} || Click to{' '}
+                  {thought.reactionCount ? 'see' : 'start'} the discussion!
+                </p>
+              </Link>
+            </div>
           </div>
         ))}
     </div>
